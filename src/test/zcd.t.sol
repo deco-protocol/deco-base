5afc70e3999cb7bfa9ddd051f944d64ed2cf0a87
Vamsi Alluri <hi@vamsiraju.com>
2019-12-20 18:07:38 -0500

    add tests for issue and withdraw

--- src/test/zcd.t.sol
+++ src/test/zcd.t.sol
@@ -11,6 +11,120 @@ contract Hevm {
     function warp(uint256) public;
 }
 
+contract User {
+    ZCD zcd;
+
+    constructor(ZCD zcd_) public {
+        zcd = zcd_;
+    }
+}
+
 contract ZCDTest is DSTest {
-    
+    Hevm hevm;
+
+    Vat vat;
+    Pot pot;
+    Dai dai;
+    DaiJoin adapter;
+
+    ZCD zcd;
+
+    address vow;
+    address self;
+
+    uint constant DATE = 1574035200;
+    uint constant RATE = 1000000564701133626865910626; // 5% / day
+
+    uint constant RAY = 10 ** 27;
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+    function rad(uint wad_) internal pure returns (uint) {
+        return wad_ * RAY;
+    }
+    function wad(uint rad_) internal pure returns (uint) {
+        return rad_ / RAY;
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = mul(x, y) / RAY;
+    }
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = mul(x, RAY) / y;
+    }
+    function rdivup(uint x, uint y) internal pure returns (uint z) {
+        // always rounds up
+        z = add(mul(x, RAY), sub(y, 1)) / y;
+    }
+
+    function day(uint number) internal returns (uint day_) {
+        day_ = add(DATE, number);
+    }
+
+    function setUp() public {
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(DATE);
+
+        vat = new Vat();
+        pot = new Pot(address(vat));
+        dai = new Dai(99);
+        adapter = new DaiJoin(address(vat), address(dai));
+
+        vat.rely(address(pot));
+        dai.rely(address(adapter));
+        self = address(this);
+
+        vow = address(bytes20("vow"));
+        pot.file("vow", vow);
+
+        vat.hope(address(pot));
+        vat.hope(address(adapter));
+
+        zcd = new ZCD(address(dai), address(adapter), address(pot));
+
+        dai.approve(address(adapter), uint(-1));
+        dai.approve(address(zcd), uint(-1));
+
+        vat.suck(self, self, rad(200 ether));
+        adapter.exit(address(this), 100 ether);
+
+        pot.file("dsr", uint(RATE)); // file at DATE
+        hevm.warp(day(1));
+    }
+
+    function test_issue_zcd_and_dcp() public {
+        uint val = rdiv(10 ether, pot.drip());
+
+        zcd.issue(self, day(2), val);
+
+        bytes32 zcdClass = keccak256(abi.encodePacked(day(2)));
+        bytes32 dcpClass = keccak256(abi.encodePacked(day(1), day(2)));
+
+        assertEq(zcd.zcd(self, zcdClass), rmul(val, pot.drip()));
+        assertEq(zcd.dcp(self, dcpClass), val);
+    }
+
+    function test_withdraw_dai_before_expiry() public {
+        uint val = rdiv(10 ether, pot.drip());
+
+        zcd.issue(self, day(3), val);
+        hevm.warp(day(2));
+
+        bytes32 zcdClass = keccak256(abi.encodePacked(day(3)));
+        bytes32 dcpClass1 = keccak256(abi.encodePacked(day(1), day(3)));
+        bytes32 dcpClass2 = keccak256(abi.encodePacked(day(2), day(3)));
+
+        zcd.claim(self, day(1), day(3), now);
+        zcd.withdraw(self, day(3), zcd.dcp(self, dcpClass2));
+
+        assertEq(zcd.zcd(self, zcdClass), 1 wei); // dcp balance 1 wei lower after claim due to rounding
+        assertEq(zcd.dcp(self, dcpClass1), 0 ether);
+        assertEq(zcd.dcp(self, dcpClass2), 0 ether);
+        assertEq(dai.balanceOf(self), rmul(val, pot.drip()) + 90 ether - 2 wei);
+    }
 }
